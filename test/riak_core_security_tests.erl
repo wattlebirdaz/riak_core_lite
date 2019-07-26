da14b6928a9e8bcf481465cc67ce10ee2832b809
Albert Schimpf <git@schimpf.link>
2019-07-26 10:18:46 +0200

    Fixed race condition in riak core security tests

--- test/riak_core_security_tests.erl
+++ test/riak_core_security_tests.erl
@@ -17,17 +17,14 @@ start_manager(Node) ->
     TreeDir = Dir ++ "/trees",
     {ok, Tree} = riak_core_metadata_hashtree:start_link(TreeDir),
     {ok, Bcst} = riak_core_broadcast:start_link([node()], [], [], []),
-    unlink(Bcst),
-    unlink(Tree),
-    unlink(Mgr),
 
     application:set_env(riak_core, permissions, [{riak_kv,[get,put]}]),
     {Mgr, Tree, Bcst}.
 
 stop_manager({Mgr, Tree, Bcst}) ->
-    catch exit(Mgr, kill),
-    catch exit(Tree, kill),
-    catch exit(Bcst, kill),
+  gen_server:stop(Mgr),
+  gen_server:stop(Tree),
+  gen_server:stop(Bcst),
     ok.
 
 security_test_() ->
