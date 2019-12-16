ceee2e0f5aa74df9245d008c5f40d129451856be
Albert Schimpf <git@schimpf.link>
2019-12-16 16:49:29 +0100

    formatting

--- src/riak_core_vnode_worker.erl
+++ src/riak_core_vnode_worker.erl
@@ -22,19 +22,20 @@
 
 -include("riak_core_vnode.hrl").
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
-        code_change/3]).
+% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+% API
 -export([start_link/1, handle_work/3, handle_work/4]).
 
 -type mod_state() :: term().
 
 -record(state, {
-        module :: atom(),
-        modstate :: mod_state()
-    }).
+    module :: atom(),
+    modstate :: mod_state()
+}).
 
--callback init_worker(partition(), Args :: term(), Props :: [{atom(), term()}]) ->
-    {ok, mod_state()}.
+-callback init_worker(partition(), Args :: term(), Props :: [{atom(), term()}]) -> {ok, mod_state()}.
 -callback handle_work(Work :: term(), sender(), mod_state()) ->
     {reply, Reply :: term(), mod_state()} |
     {noreply, mod_state()}.
@@ -44,34 +45,39 @@ start_link(Args) ->
     [VNodeIndex, WorkerArgs, WorkerProps, Caller] = proplists:get_value(worker_args, Args),
     gen_server:start_link(?MODULE, [WorkerMod, VNodeIndex, WorkerArgs, WorkerProps, Caller], []).
 
+
 handle_work(Worker, Work, From) ->
     handle_work(Worker, Work, From, self()).
 
+
 handle_work(Worker, Work, From, Caller) ->
     gen_server:cast(Worker, {work, Work, From, Caller}).
+
 
 init([Module, VNodeIndex, WorkerArgs, WorkerProps, Caller]) ->
     {ok, WorkerState} = Module:init_worker(VNodeIndex, WorkerArgs, WorkerProps),
     %% let the pool queue manager know there might be a worker to checkout
     riak_core_vnode_worker_pool:worker_started(Caller),
-    {ok, #state{module=Module, modstate=WorkerState}}.
+    {ok, #state{module = Module, modstate = WorkerState}}.
+
 
 handle_call(Event, _From, State) ->
     logger:debug("Vnode worker received synchronous event: ~p.", [Event]),
     {reply, ok, State}.
 
+
 handle_cast({work, Work, WorkFrom, Caller},
-            #state{module = Mod, modstate = ModState} = State) ->
+    #state{module = Mod, modstate = ModState} = State) ->
     NewModState = case Mod:handle_work(Work, WorkFrom, ModState) of
-        {reply, Reply, NS} ->
-            riak_core_vnode:reply(WorkFrom, Reply),
-            NS;
-        {noreply, NS} ->
-            NS
-    end,
+                      {reply, Reply, NS} ->
+                          riak_core_vnode:reply(WorkFrom, Reply),
+                          NS;
+                      {noreply, NS} ->
+                          NS
+                  end,
     %% check the worker back into the pool
     riak_core_vnode_worker_pool:checkin_worker(Caller, self()),
-    {noreply, State#state{modstate=NewModState}};
+    {noreply, State#state{modstate = NewModState}};
 
 handle_cast(_Event, State) -> {noreply, State}.
 
